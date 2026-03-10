40ac48fce510411a1264adf05cecb999ab27078b
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2026-03-10 13:46:19 +0100

    Switch face on mode down

--- watch-faces/complication/timer_face.c
+++ watch-faces/complication/timer_face.c
@@ -340,11 +340,6 @@ bool timer_face_loop(movement_event_t event, void *context) {
         case EVENT_ALARM_REALLY_LONG_UP:
             _abort_quick_cycle(state);
             break;
-        case EVENT_MODE_BUTTON_DOWN:
-            break;
-        case EVENT_MODE_BUTTON_UP:
-            movement_move_to_next_face();
-            break;
         case EVENT_MODE_LONG_PRESS:
         case EVENT_TIMEOUT:
             _abort_quick_cycle(state);
