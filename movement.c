611000eb8d0c01df836ed91c5ee78adc86a3d0f2
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2026-04-03 14:25:53 +0200

    Use classic alarm sequence

--- movement.c
+++ movement.c
@@ -113,15 +113,12 @@ static int8_t *_pending_sequence;
 
 // The note sequence of the default alarm
 int8_t alarm_tune[] = {
-    BUZZER_NOTE_C8, 3,
-    BUZZER_NOTE_REST, 4,
-    BUZZER_NOTE_C8, 3,
-    BUZZER_NOTE_REST, 4,
-    BUZZER_NOTE_C8, 3,
-    BUZZER_NOTE_REST, 4,
-    BUZZER_NOTE_C8, 5,
-    BUZZER_NOTE_REST, 38,
-    -8, 9,
+    BUZZER_NOTE_C8, 8,
+    BUZZER_NOTE_REST, 8,
+    BUZZER_NOTE_C8, 8,
+    BUZZER_NOTE_REST, 8,
+    BUZZER_NOTE_REST, 32,
+    -5, 9,
     0
 };
 
