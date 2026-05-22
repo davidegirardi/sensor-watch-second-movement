e36555924909f1389efc8d6c58128d4cd0d8a39d
David Volovskiy <devolov@gmail.com>
2026-05-22 08:19:43 -0400

    We don't even include the event_minute on the faces that update the time if they're not a G-Shock LCD

--- watch-faces/complication/festival_schedule_face.c
+++ watch-faces/complication/festival_schedule_face.c
@@ -638,6 +638,7 @@ bool festival_schedule_face_loop(movement_event_t event, void *context) {
                 _display_screen(state);
             }
             break;
+#ifdef FORCE_GSHOCK_LCD_TYPE
         case EVENT_MINUTE:
             if (!state->cyc_through_all_acts && 
                 (state->curr_screen == FESTIVAL_SCHEDULE_SCREEN_ACT || 
@@ -645,6 +646,7 @@ bool festival_schedule_face_loop(movement_event_t event, void *context) {
                 gshock_display_current_time_top_right();
             }
             break;
+#endif
         case EVENT_TIMEOUT:
             if (state->cyc_through_all_acts){
                 state->cyc_through_all_acts = false;
